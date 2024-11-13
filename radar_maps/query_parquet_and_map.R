b6c3b038993210166484e30e3cac7c311699f4d0
cfurl <chad.furl@gmail.com>
2024-11-13 08:24:14 -0600

    add ws boundaries

--- radar_maps/query_parquet_and_map.R
+++ radar_maps/query_parquet_and_map.R
@@ -7,6 +7,7 @@ library(magick)
 library(ggplot2)
 library(units)
 library(ggplot2)
+library(zoo)
 
 
 # identify the root folder of your parquet files
@@ -36,7 +37,7 @@ toc()
 # Join the just completed query with ST4 bins using 'grib_id'
 
 streams <- read_sf(".\\gis\\boundaries_features\\streams_recharge.shp")
-bin_outline <- read_sf(".\\gis\\clipped_hrap\\usgs_recharge_basins\\usgs_dissolved.shp") 
+ws_outline <- read_sf(".\\gis\\boundaries_features\\usgs_basins.shp") 
 map <- read_sf(".\\gis\\clipped_hrap\\usgs_recharge_basins\\usgs_dissolved.shp") |>
   #map <- read_sf("C:\\texas_mpe\\arrow\\gis\\texas_grib_bins_clipped.shp") |>
   clean_names() |>
@@ -59,28 +60,64 @@ plot_box <- tibble(xmin = st_bbox(map)[1],
                    xrange = xmax - xmin,
                    yrange = ymax - ymin) 
 
+{
+linear_radar_scale_low_break <- function (min, max, bin_number, first_break) {
+  
+  colours <- as_tibble(c(NA,"#0826A2","#22FE05","#2CAC1B","#248418", "#F6FB07", "#FFE890", "#FFC348","#E01E17", "#A92B26","#8C302C","#CC17DA", "#AE6DB3","#8798C6" )) |>
+    slice(1:(bin_number))
+  
+  breaks <- as_tibble(seq(min,max,max/bin_number)) |>
+    slice(2:n()) |>
+    slice(1:(n()-1))|>
+    add_row(value = first_break, .before = 1)
+  
+  breaks_w_lims <- as_tibble(seq(min,max,max/bin_number))  |>
+    add_row(value = first_break, .before = 2) 
+  
+  values <- rollmean(breaks_w_lims,2)
+  breaks <- as.numeric(unlist(breaks))  
+  
+  output <- list (colours, min, max, breaks, values)
+  
+}
 
+a <- linear_radar_scale_low_break (0,200,10,5)
 
+colours <- unlist(a[1])
+min <- unlist(a[2])
+max <- unlist(a[3])
+breaks <- unlist(a[4])
+values <-  unlist(a[5])
 
-
-
+}
 
 
 p1 <- ggplot() +
   
   geom_sf(data = map, aes(fill = sum_rain), linewidth = .05)+
-  
   scale_fill_stepsn(name = "mm",
-                    colours=c("pink",NA,"#22FE05","#2CAC1B","#248418", "#F6FB07", "#FFE890", "#FFC348","#E01E17", "#A92B26","#8C302C","#CC17DA", "#AE6DB3","orange" ), 
-                    breaks = c(2,10,20,30,40,50,60,70,80,90,100,110,120),
-                    limits=c(0,130),
-                    values = scales::rescale(c(1,5, 15, 25, 35, 45, 55, 65, 75, 85, 95, 105, 115, 125), 
-                                             from = c(0, 130)), 
+                    colours=c(colours), 
+                    breaks = c(breaks),
+                    limits=c(min,max),
+                    values = scales::rescale(c(values), 
+                                             from = c(min, max)), 
                     show.limits = FALSE,
                     na.value = "#F1F4FC")  +
   
+  
+  
+  #scale_fill_stepsn(name = "mm",
+   #                 colours=c("pink",NA,"#22FE05","#2CAC1B","#248418", "#F6FB07", "#FFE890", "#FFC348","#E01E17", "#A92B26","#8C302C","#CC17DA", "#AE6DB3","orange" ), 
+    #                breaks = c(2,10,20,30,40,50,60,70,80,90,100,110,120),
+     #               limits=c(0,130),
+      #              values = scales::rescale(c(1,5, 15, 25, 35, 45, 55, 65, 75, 85, 95, 105, 115, 125), 
+       #                                      from = c(0, 130)), 
+        #            show.limits = FALSE,
+         #           na.value = "#F1F4FC")  +
+  
   ggtitle("2015 Precipitation") +
   geom_sf(data = streams, fill=NA, color="blue") +
+  geom_sf(data = ws_outline, fill=NA, color="black", linewidth = .05) +
   annotation_raster(logo, 
                     # Position adjustments here using plot_box$max/min/range
                     ymin = plot_box$ymin + 1,

107e3567a8aadbb8e7db2172698afe2a7f4c32dc
Chad Furl laptop <chad.furl@gmail.com>
2024-11-21 15:53:26 -0600

    cleaning for initial mtg w tanya ggmap added

--- radar_maps/query_parquet_and_map.R
+++ radar_maps/query_parquet_and_map.R
@@ -1,18 +1,9 @@
-library(arrow)
-library(dplyr)
-library(tictoc)
-library(sf)
-library(janitor)
-library(magick)
-library(ggplot2)
-library(units)
-library(ggplot2)
-library(zoo)
-
+packages<- c("arrow", "dplyr", "tictoc", "sf", "janitor", "magick", "ggplot2", "units", "zoo", "ggmap")
+lapply(packages, require, character.only = TRUE)
 
 # identify the root folder of your parquet files
-#month_parq <- "C:\\texas_mpe\\arrow\\st4_parq"
-month_parq <- ".\\parquet\\parquet_files"
+#month_parq <- "C:\\texas_mpe\\arrow\\st4_parq"  # full repository here
+month_parq <- ".\\parquet\\parquet_files"        # September 2015 on github
 
 # establish your connection, this is the time to see your file system schema and make any adjustments
 tx_rain <- open_dataset(month_parq)
@@ -23,11 +14,10 @@ sum_rain_query <- tx_rain %>%
   filter(year==2015) %>%
   group_by (grib_id) %>%
   summarize(
-    sum_rain = sum(rain_mm, na.rm=TRUE)
-  ) %>%
+    sum_rain = sum(rain_mm, na.rm=TRUE)) %>%
   arrange(desc(sum_rain))
 
-# collect the query when you are ready (grouped and summed over 120 million rows in under three minutes)
+# collect the query when you are ready 
 tic()
 sum_rain_collect <- collect(sum_rain_query)
 toc()
@@ -40,6 +30,7 @@ map <- read_sf(".\\gis\\clipped_hrap\\usgs_recharge_basins\\usgs_dissolved.shp")
   clean_names() |>
   left_join(sum_rain_collect, by = "grib_id" )
 
+# drop your geometry and make some volume and linear rainfall calculations
 map_math <- map |> st_drop_geometry() |>
   mutate(cubic_m_precip = bin_area * sum_rain * .001)
 
@@ -48,17 +39,13 @@ bap <- sum(map_math$cubic_m_precip) * 39.37/sum(map_math$bin_area) # 39.37 is me
 # read in other items to map - dress up figure
 streams <- read_sf(".\\gis\\boundaries_features\\streams_recharge.shp")
 ws_outline <- read_sf(".\\gis\\boundaries_features\\usgs_basins.shp") 
+lakes <- read_sf(".\\gis\\boundaries_features\\reservoirs.shp")
 
-
+# raster images
+radar_tag <- image_read(path='.\\gis\\logo\\radar_tag.png')
 logo <- image_read(path='.\\gis\\logo\\EAHCP_color_vertical logo.png')
-# create a plot box to help you position your logo
-plot_box <- tibble(xmin = st_bbox(map)[1],
-                   xmax = st_bbox(map)[3],
-                   ymin = st_bbox(map)[2],
-                   ymax = st_bbox(map)[4],
-                   xrange = xmax - xmin,
-                   yrange = ymax - ymin) 
 
+# function to generate radar scale with 2 breaks
 linear_radar_scale_two_break <- function (min, max, bin_number_minus_2, first_break, second_break) {
   
   # min <- 0
@@ -87,45 +74,57 @@ linear_radar_scale_two_break <- function (min, max, bin_number_minus_2, first_br
   
 }
 
+# run function and unlist values to populate 'scale_fill_stepsn'
 a <- linear_radar_scale_two_break (0,200,8,2,6)
+          colours <- unlist(a[1])
+          min <- unlist(a[2])
+          max <- unlist(a[3])
+          breaks <- unlist(a[4])
+          values <-  unlist(a[5])
 
-colours <- unlist(a[1])
-min <- unlist(a[2])
-max <- unlist(a[3])
-breaks <- unlist(a[4])
-values <-  unlist(a[5])
+# grab your tiles from Stadia.  Your API key is hard written in your .Renviron.
+ear <- get_stadiamap(bbox = c(left = -100.85, bottom = 29.0, 
+                              right = -97.75, top = 30.4), 
+                     zoom = 9,
+                     maptype = "alidade_smooth_dark",
+                     crop = TRUE)
 
-
-
-
-
-p1 <- ggplot() +
+p1 <- ggmap(ear) +
   
-  geom_sf(data = map, aes(fill = sum_rain), linewidth = .05)+
-  scale_fill_stepsn(name = "mm",
+  geom_sf(data = map, aes(fill = sum_rain), alpha = .6, linewidth = .05,  inherit.aes = FALSE)+ 
+              scale_fill_stepsn(name = "mm",
                     colours=c(colours), 
                     breaks = c(breaks),
                     limits=c(min,max),
                     values = scales::rescale(c(values), from = c(min, max)), 
                     show.limits = FALSE,
                     na.value = "#F1F4FC")  +
+  guides(colour = guide_legend(override.aes = list(alpha = 0.6)))+ # trying to apply alpha to legend. Doesn't work...
   
-  #ggtitle("2015 Precipitation") +
-  geom_sf(data = streams, fill=NA, color="blue", linewidth = .1) +
- 
-  annotation_raster(logo, # native logo is 796 x 1168
+  geom_sf(data = streams, fill=NA, color="blue", linewidth = .1,   inherit.aes = FALSE) +
+  geom_sf(data = lakes,  fill = "blue", linewidth = .1,   inherit.aes = FALSE) +
+  
+  annotation_raster(logo, # native logo is 796 (wide) x 1168 (tall)
                     # Position adjustments here using plot_box$max/min/range
-                    ymin = plot_box$ymin ,
-                    ymax = plot_box$ymin + .27 , 
-                    xmin = plot_box$xmin ,
-                    xmax = plot_box$xmin + .18) +
-  annotate(geom="text",x= -99,y=29.28,label="September 2015 Precipitation",size=3,hjust=0)+
-  annotate(geom="text",x= -99,y=29.2,label= paste0("Basin average of ", sprintf("%0.2f", bap), " in"),size=3,hjust=0)+
+                    ymin = 29.05 ,
+                    ymax = 29.05 + .27 , 
+                    xmin = -97.80 ,
+                    xmax = -97.80 - .18) +
+  annotation_raster(radar_tag, # native logo is 796 (wide) x 1168 (tall)
+                    # Position adjustments here using plot_box$max/min/range
+                    # https://onlinepngtools.com/convert-text-to-png
+                    ymin = 29.02 ,
+                    ymax = 29.05 + .51 , 
+                    xmin = -98.10 ,
+                    xmax = -98.10 - .33) +
+  annotate(geom="text",x= -100.75,y=30.28,label="Edwards Aquifer Recharge Zone",size=4,hjust=0, color = "#F1F4FC")+
+  annotate(geom="text",x= -99,y=29.28,label="September 2015 Precipitation",size=3,hjust=0, color = "#F1F4FC")+
+  annotate(geom="text",x= -99,y=29.2,label= paste0("Basin average of ", sprintf("%0.2f", bap), " in"),size=3,hjust=0, color = "#F1F4FC")+
   
   theme(legend.key.height = unit(1.0, "cm"),
         legend.margin = margin(0,0,0,0),
         #legend.position = "left",
-        legend.position = c(-.01,.5),
+        legend.position = c(.05,.5),
         legend.box.margin=margin(0,2,0,5),
         plot.margin=unit(c(0,0,0,0), 'cm'),
         axis.title = element_blank(),
@@ -135,9 +134,10 @@ p1 <- ggplot() +
         axis.ticks = element_blank(),
         plot.background = element_rect(color = "#F1F4FC", fill = "#F1F4FC"),
         panel.grid.major = element_blank(),
-        legend.background= element_rect(fill = "#F1F4FC"),
-        legend.text = element_text(size = 5),
-        legend.title = element_text(size = 7),
+        #legend.background= element_rect(fill = "#F1F4FC"),
+        legend.background= element_rect(fill = NA),
+        legend.text = element_text(size = 5, color = "#F1F4FC"),
+        legend.title = element_text(size = 7, color = "#F1F4FC"),
         panel.background = element_rect(fill = "#F1F4FC"))
 
 
